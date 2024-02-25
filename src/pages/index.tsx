5f563d8d4630423987d0526fae35dbec53a6f75e
David Fung <davidfung@Davids-MacBook-Pro-3.local>
2024-02-25 11:46:15 -0700

    this is my first commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -28,7 +28,7 @@ export default function Home() {
             >
               By{' '}
               <Image
-                src="/vercel.svg"
+                src="/sporkwhale.png"
                 alt="Vercel Logo"
                 className={styles.vercelLogo}
                 width={100}
@@ -42,7 +42,7 @@ export default function Home() {
         <div className={styles.center}>
           <Image
             className={styles.logo}
-            src="/next.svg"
+            src="/sporkwhale.png"
             alt="Next.js Logo"
             width={180}
             height={37}
